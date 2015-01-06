3d25cd877f875db5809dc65cea0d70d4319e0d5f
Yvette <yvettecook@gmail.com>
2015-01-06 11:09:51 +0000

    User stories sketched out

--- goatlists/functional_tests.py
+++ goatlists/functional_tests.py
@@ -3,4 +3,29 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
+# Even goats have things to do, so they use GoatList
+# Tessa the Testing goat goes to check out the homepage
 assert 'Django' in browser.title
+
+# On the page there is a title and a header.
+assert 'Goat-Do' in browser.title
+
+# She is invited to enter a to-do list item.
+
+# She types 'Climb up a Tree'.
+
+# When she hits enter, the page updates, and now the page lists
+# '1: Climb up a Tree' as an item on the to-do list.
+
+# There is still a textbox inviting her to enter another to-do item
+# She enters 'Don't fall off tree'.
+
+# The page updates again, and now shows both items on her list.
+
+# She wants to open her to-dos on another computer, and notices that the site
+# has generated a unique url for her list, and there is text on the page
+# explaining this.
+
+# She visits the page in another browser window, and wow! Her todo list is there!
+
+browser.quit
